34e115d95ecf10d8d05b4b02b388c464d2ece0df
Alex Bennett <alex.bennett@productscience.ai>
2023-06-21 08:57:33 -0600

    adjusting font, adding break points

--- app/page.tsx
+++ app/page.tsx
@@ -2,84 +2,86 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-6">
+    <main className="flex min-h-screen flex-col items-center p-6 xs:p-2">
       <h1 className="text-[48px]">Alex Bennett</h1>
-      <section className="margin-auto pt-[20px] pb-[40px]">
-        <p className="text-[20px] text-center max-w-[580px] indent-4">
+      <section className="margin-auto pt-[20px] text-[20px] pb-[40px] max-w-[580px]">
+        <p className=" text-center indent-4">
           Software Engineer & AWS Certified Cloud Architect with over a decade
           of experience building comprehensive technology solutions across many
           industries.
         </p>
-        <p className="text-center text-[20px] max-w-[580px] pt-[20px]">
+        <p className="text-center  pt-[20px]">
           OSS contributor, conference speaker, and life-long learner.
         </p>
-        <p className="text-center text-[20px] max-w-[580px] pt-[20px]">
+        <p className="text-center  pt-[20px]">
           Let&apos;s build something great, together.
         </p>
         <div className="margin-auto flex flex-col justify-center">
           <a
             href="/Resume-Alex-Bennett.pdf"
             download="alex-bennett-resume.pdf"
-            className="my-[20px] text-[20px] underline text-center text-blue-700 m-auto"
+            className="my-[20px]  underline text-center text-blue-700 m-auto"
           >
             Download Resume
           </a>
         </div>
       </section>
-      <section className="w-[90%] flex flex-wrap items-baseline justify-evenly margin-auto max-w-[1100px]">
-        <div className="text-[20px] pt-[20px] w-[500px]">
-          <p className="font-medium pb-[10px]">
+      <section className="text-left text-[20px] xs:text-[16px] xs:w-[98%] md:w-[98%] w-[90%] flex flex-wrap items-baseline justify-evenly margin-auto max-w-[1100px]">
+        <div className="pt-[20px] w-[500px]">
+          <p className="font-medium text-[18px] pb-[10px]">
             🧰 Tools, libraries, and frameworks:
           </p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+          <p className="indent-4 text-left">
             React (Next.js/Remix), Qwik, Node.js, TypeScript, GraphQL/Apollo,
             Webpack, Rust, Wasm and many more not listed here.
           </p>
         </div>
-        <div className="w-[500px] text-[20px] max-w-[580px] text-left pt-[20px]">
-          <p className="font-medium pb-[10px]">🧩 Patterns:</p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+        <div className="w-[500px] text-left pt-[20px]">
+          <p className="font-medium text-[18px] pb-[10px]">🧩 Patterns:</p>
+          <p className="indent-4 text-left">
             Serverless, Micro frontends, Event Sourcing and Event Driven
             Architecture, CQRS, functional programming, SOLID principles, clean
             architecture, BFF, Service-full integration, Polylith and many more.
           </p>
         </div>
-        <div className="w-[500px] text-[20px] max-w-[580px] text-left pt-[20px]">
-          <p className="font-medium pb-[10px]">🧠 Knowledgable about:</p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+        <div className="w-[500px] text-left pt-[20px]">
+          <p className="font-medium text-[18px] pb-[10px]">
+            🧠 Knowledgable about:
+          </p>
+          <p className="indent-4 text-left">
             DevOps, Compilers, Frameworks, Runtimes, Javascript toolchain,
             Testing, Monorepos, Performance Engineering, Observability, Browser
             APIs, State Management, Design Systems, and many more.
           </p>
         </div>
 
-        <div className="w-[500px] text-[20px] max-w-[580px] text-left pt-[20px]">
-          <p className="font-medium pb-[10px]">⛅️ Clouds:</p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+        <div className="w-[500px] text-left pt-[20px]">
+          <p className="font-medium text-[18px] pb-[10px]">⛅️ Clouds:</p>
+          <p className="indent-4 text-left">
             AWS, GCP, Cloudflare, Vercel, Netlify, Heroku, etc
           </p>
         </div>
 
-        <div className="w-[500px] text-[20px] max-w-[580px] text-left pt-[20px]">
-          <p className="font-medium pb-[10px]">🗄 Databases:</p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+        <div className="w-[500px] text-left pt-[20px]">
+          <p className="font-medium text-[18px] pb-[10px]">🗄 Databases:</p>
+          <p className="indent-4 text-left">
             Realtional: Postgres, MySQL, Nonrelational: DynamoDB, MongoDB,
             Redis. Also have worked with Columnar and Graph DBs.
           </p>
         </div>
-        <div className="w-[500px] text-[20px] max-w-[580px] text-left pt-[20px]">
-          <p className="font-medium pb-[10px]">✏️ Design:</p>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+        <div className="w-[500px] text-left pt-[20px]">
+          <p className="font-medium text-[18px] pb-[10px]">✏️ Design:</p>
+          <p className="indent-4 text-left">
             Figma, SketchUp, Adobe: Photoshop, Illustrator, After Effects,
             Premier Pro
           </p>
         </div>
       </section>
 
-      <div className="w-[90%] flex flex-wrap items-baseline justify-evenly margin-auto max-w-[1100px]">
+      <div className="xs:w-[98%] md:w-[98%] w-[90%] flex flex-wrap items-baseline justify-evenly margin-auto max-w-[1100px]">
         <section className="flex flex-col justify-center margin-auto w-[500px]">
           <h2 className="text-[48px] py-[40px] text-center">Writing</h2>
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+          <p className="indent-4  text-left">
             As part of the Module Federation OSS community team, I worked with
             Zack Jackson to write a webpack plugin to syncronize multiple,
             independent builds using Module Federation and I wrote about it{' '}
@@ -97,7 +99,7 @@ export default function Home() {
             height={500}
             className="py-[20px]"
           />
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+          <p className="indent-4  text-left">
             This OSS project was later published in the seminal book on
             Micro-Frontends, written by Principal AWS Engineer, Luca Mezzalira.{' '}
           </p>
@@ -108,7 +110,7 @@ export default function Home() {
             height={500}
             className="py-[20px]"
           />
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+          <p className="indent-4  text-left">
             As an early champion and adopter of MFE and Module federation, I
             gave a talk at Develop Denver in 2019 about the MFE archietcture I
             built for a 8Bn dollar company.{' '}
@@ -120,7 +122,7 @@ export default function Home() {
             height={250}
             className="py-[20px] m-auto"
           />
-          <p className="indent-4 text-[18px] max-w-[580px] text-left">
+          <p className="indent-4  text-left">
             <a
               href="https://docs.google.com/presentation/d/1rbGS8uENrHhL1FJi_PIQ89V_gRzag8v3QArVqACg56Y/edit#slide=id.p"
               className="text-blue-700 underline"
@@ -138,62 +140,17 @@ export default function Home() {
             className="py-[20px] m-auto"
           />
           <Image
-            src="/diagram-slide.png"
+            src="/diagram-close.png"
             alt="FMR logo"
             width={500}
-            height={300}
+            height={750}
             className="py-[20px] m-auto"
           />
         </section>
         <section className="flex flex-col justify-center margin-auto w-[500px]">
           <h2 className="text-[48px] py-[40px] text-center">Projects</h2>
-          <div className="text-[18px] max-w-[580px] text-left">
-            <h3 className="text-[24px]">
-              <a
-                href="https://mewzik-viz.vercel.app/"
-                className="text-blue-700 underline"
-              >
-                Music Visualizer
-              </a>
-            </h3>
-            <div className="flex flex-wrap">
-              <Image
-                src="/music-viz-red.png"
-                alt="FMR logo"
-                width={250}
-                height={250}
-                className="py-[20px] m-auto"
-              />
-              <Image
-                src="/music-viz-blue.png"
-                alt="FMR logo"
-                width={250}
-                height={250}
-                className="py-[20px] m-auto"
-              />
-            </div>
-            <p className="indent-4 ">
-              Built with three.js, Web Audio APIs, and knowledge of trigonometry
-              sound physics.
-              <br />
-              <br />
-              It works by taking music as input data and the program will get a
-              set amount of frequencies. For each frequency, it will get the
-              amplitude, and it will use this information to create a visual
-              representation of the music. The visualizer requires an MP3 to
-              play. Here is a soundbyte MP3 of Beethovens 5th Symphony if you do
-              not have one available.
-              <br />
-              <br />
-              <a
-                href="beethoven-symphony-5.mp3"
-                className="text-blue-700 underline"
-              >
-                Download MP3
-              </a>
-            </p>
-          </div>
-          <div className="pt-[20px] text-[18px] max-w-[580px] text-left">
+
+          <div className="pt-[20px]  text-left">
             <h3 className="text-[24px]">
               <a
                 href="https://boggle.pages.dev/"
@@ -225,7 +182,7 @@ export default function Home() {
               <br />
             </p>
           </div>
-          <div className="text-[18px] max-w-[580px] text-left">
+          <div className=" text-left">
             <h3 className="text-[24px]">
               <a
                 href="https://mewzik-viz.vercel.app/"
@@ -252,7 +209,7 @@ export default function Home() {
             </div>
             <p className="indent-4 ">
               Built with three.js, Web Audio APIs, and knowledge of trigonometry
-              sound physics.
+              and sound physics.
               <br />
               <br />
               It works by taking music as input data and the program will get a
@@ -271,7 +228,7 @@ export default function Home() {
               </a>
             </p>
           </div>
-          <div className="pt-[20px] text-[18px] max-w-[580px] text-left">
+          <div className="pt-[20px]  text-left">
             <h3 className="text-[24px]">
               <a
                 href="https://github.com/alexUXUI/wasm-federation-demo"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1200px',
+      '2xl': '1440px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -14,5 +22,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [],
-}
+};
