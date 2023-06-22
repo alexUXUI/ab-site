c0e97d9a761882717be8e7e30f06ace1540c6df8
Alex Bennett <alex.bennett@productscience.ai>
2023-06-22 10:15:25 -0600

    adds analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css';
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GQ5QV9THCS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GQ5QV9THCS');
+        `}
+      </Script>
     </html>
   );
 }
